b35013f8a09f2784dc3dbd2b5084eed7c17eb876
Evan C. Wright <evancwright@yahoo.com>
2020-05-03 14:03:51 +0100

    initial commit

---
+++ Lantern/LASMCmd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LASM;
+
+namespace LASMCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly lasm = new Assembly(args[0]);
+            lasm.Assemble();
+        }
+    }
+}
